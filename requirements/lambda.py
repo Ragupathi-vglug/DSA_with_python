0a5b72419970598faffcb428363994f1fa2855a0
MADARA143390 <ragu26555@gmail.com>
2026-06-13 23:27:12 +0530

    Requiremnts of dsa has been uploaded

---
+++ requirements/lambda.py
@@ -0,0 +1,12 @@
+# lambda argument:expresssion
+square=lambda a:a*a
+print(square(5))
+
+# Maximum
+num=lambda x,y:x if x>y else y
+print(num(100,20))
+
+students=[("Ragu",90),("pathi",80),("chelsie",99)]
+print("Before ",students)
+students.sort(key=lambda x:x[1])
+print("After :",students)
