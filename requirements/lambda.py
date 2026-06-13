a420a91b8effe9b99c71de0e3ab44762da97f066
MADARA143390 <ragu26555@gmail.com>
2026-06-13 23:28:59 +0530

    Requiremnts of dsa has been uploaded

--- requirements/lambda.py
+++ requirements/lambda.py
@@ -2,6 +2,10 @@
 square=lambda a:a*a
 print(square(5))
 
+# Using two arguments
+n=lambda a,b:a+b
+print(n)
+
 # Maximum
 num=lambda x,y:x if x>y else y
 print(num(100,20))
@@ -10,3 +14,4 @@ students=[("Ragu",90),("pathi",80),("chelsie",99)]
 print("Before ",students)
 students.sort(key=lambda x:x[1])
 print("After :",students)
+
