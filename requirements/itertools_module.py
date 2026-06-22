5415e2c329613325a62074060e41eec615c6d871
MADARA143390 <ragu26555@gmail.com>
2026-06-22 21:31:09 +0530

    Files added

--- requirements/itertools_module.py
+++ requirements/itertools_module.py
@@ -1,9 +1,22 @@
-from itertools import count
+from itertools import *
 
-# Count()
-for i in count(5):
-    if i==1000:
-        break
-    print(i)
+# # Count()
+# for i in count(5):
+#     if i==1000:
+#         break
+#     print(i)
 
-# cycle()
+# # cycle()
+# for i in cycle("ABC"):
+#     print(i)
+
+# # repeat()
+# for i in repeat("Ragu",10):
+#     print(i)
+
+# permutation()
+print(list(permutations([1,2,3])))
+
+# combinational
+print(list(combinations([1,2,3],2)))
+
