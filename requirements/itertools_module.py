2b465649eeecdbed60addc745a6ed57428b0f688
MADARA143390 <ragu26555@gmail.com>
2026-06-22 21:46:18 +0530

    Files added

--- requirements/itertools_module.py
+++ requirements/itertools_module.py
@@ -1,18 +1,18 @@
 from itertools import *
 
-# # Count()
-# for i in count(5):
-#     if i==1000:
-#         break
-#     print(i)
+# Count()
+for i in count(5):
+    if i==1000:
+        break
+    print(i)
 
-# # cycle()
-# for i in cycle("ABC"):
-#     print(i)
+# cycle()
+for i in cycle("ABC"):
+    print(i)
 
-# # repeat()
-# for i in repeat("Ragu",10):
-#     print(i)
+# repeat()
+for i in repeat("Ragu",10):
+    print(i)
 
 # permutation()
 print(list(permutations([1,2,3])))
@@ -20,3 +20,9 @@ print(list(permutations([1,2,3])))
 # combinational
 print(list(combinations([1,2,3],2)))
 
+# product
+for i in product([1,2,3],["A","B","C"]):
+    print(i)
+
+# accumulator
+print(list(accumulate([1,2,3,4,5])))
